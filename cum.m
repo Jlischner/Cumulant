a710d3a0b1f1ef5ebb48c2b618f89c8e7480ba00
jlischner <jlischner597@gmail.com>
2011-12-16 11:01:16 -0800

    Improvements: add first and second order expansion without working in time

--- cum.m
+++ cum.m
@@ -1,11 +1,21 @@
 load spec;
 ws = spec(:,7);
 
-elda = -0.087494;
-vxc  = -1.5844;
-
+elda = -5.956967;
+vxc  = -10.455963;
+eF = 6.5; %# fermi energy
 ReSig = spec(:,8)-vxc;
 ImSig = spec(:,10);
+
+#load output;
+#vxc = dmu*27.21;
+#ws *= 27.21;
+#elda = xi*27.21 + 0.00001;
+#eF = ef*27.21;
+#ReSig = real(Sig)*27.21 - vxc;
+#ImSig = imag(Sig)*27.21;
+
+wsN = [-45 :0.1: 2]'; %# freq. grid for cumulant A
 Sigma = ReSig + I*ImSig;
 
 A = 1/pi * abs(ImSig) ./ ( (ws-elda-ReSig).^2 + ImSig.^2);
@@ -13,6 +23,14 @@ A = 1/pi * abs(ImSig) ./ ( (ws-elda-ReSig).^2 + ImSig.^2);
 Gamma = abs( ImSig )/pi;
 dw = ws(2)-ws(1);
 
+%# compute:
+%# alphak = Im dSigma/dw(elda)
+%# gammak = Re dSigma/dw(elda)
+%# etak   = |Im Sigma(elda)|
+%# deltaE = Re Sigma(elda) - Vxc
+
+#elda_save = elda;
+#elda = -0.3179;
 if( min(ws) < elda && elda < max(ws))
   IndElda = (elda - min(ws))/dw + 1;
   distA   = IndElda - floor(IndElda);
@@ -40,8 +58,11 @@ else
   break;
 endif;
 
-#ts   = [-10 : 0.01 : 10]';
-ts   = [-2000: 1 : 2000]';
+alphak = 0.;
+#deltaE = 0.;
+#elda = elda_save;
+
+ts   = [-60: 0.1 : 60]';
 dt   = ts(2)-ts(1);
 wt   = ws*ts';
 
@@ -52,20 +73,18 @@ exptw = exp(I*ts*ws');
 
 %# compute full spectral function following Gunnarsson/Aryasetiawan
 %# have to make choice for chemical potential - middle of gap?
-eF = 0;
 CS = Gamma.*( (ws-elda) < eF )-GammaElda-(ws-elda)*DGammaElda;
+
 %# get rid of divergence at ws=elda
-minInd = floor(IndElda) - 10;
-maxInd = floor(IndElda) + 10;
+fitrange = 10;
+minInd = floor(IndElda) - fitrange;
+maxInd = floor(IndElda) + fitrange;
 P= polyfit((ws-elda)(minInd:maxInd),CS(minInd:maxInd),2);
 PN  = [P(1) 0 0];
 CSZ = polyval(PN,ws-elda);
-#wsZ = (ws-elda)(minInd:maxInd);
-#CSF = CS(minInd:maxInd);
-#a   = sum( CSF.*wsZ.^2 ) / sum(wsZ.^4);
-#CSZ2= a*(ws-elda).^2;
 
-wCut= 0.2;
+#wCut= 0.01;
+wCut = 1.5;
 fCut= exp(-(ws-elda).^2/wCut^2);
 CSN = CS  .* (1-fCut) + CSZ.*fCut;
 CS2 = CSN ./ (ws-elda).^2;
@@ -77,7 +96,6 @@ CSt = exp(I*elda*ts) .* CSt;
 expC= exp(CSt) .* expCqp;
 
 %# Fourier transform back to freq. space
-wsN = [-1:0.01:1]';
 exptwN = exp(I*ts*wsN');
 
 %# quasiparticle spectral function
@@ -86,11 +104,46 @@ Aqp  = transpose(Aqp);
 Aqp *= dt/2/pi;
 
 %# full spectral function
-Agun= sum( dmult(expC, exptwN ),1);
-Agun= transpose(Agun);
-Agun *= dt/2/pi;
+Ac = sum( dmult(expC, exptwN ),1);
+Ac = transpose(Ac);
+Ac *= dt/2/pi;
 
 %# first order expansion
-dAS = dt* sum( dmult( expCqp .* CSt, exptwN), 1);
-dAS = transpose(dAS);
-dAS /= 2*pi;
+dA = dt* sum( dmult( expCqp .* CSt, exptwN), 1);
+dA = transpose(dA);
+dA /= 2*pi;
+
+%# first order expansion without working in time
+Eqp = elda + deltaE;
+Aqpw = (etak*cos(alphak)-(ws-Eqp)*sin(alphak) )./( (ws-Eqp).^2 + etak.^2);
+Aqpw *= exp(-gammak)/pi;
+dA2 = dw*conv(Aqpw,CS2);
+
+wsC  = dw*[0:length(Aqpw)+length(CS2)-2]';
+wsC += 2*ws(1)-elda;
+AqpC = (etak*cos(alphak)-(wsC-Eqp)*sin(alphak) )./( (wsC-Eqp).^2 + etak.^2);
+AqpC *= exp(-gammak)/pi;
+A1   = AqpC + dA2;
+
+%# second order expansion without working in time
+CS2conv = dw*conv(CS2,CS2)/2; 
+wsconv  = dw*[0:2*length(CS2)-2]';
+wsconv += 2*ws(1)-2*elda;
+
+Aqpconv = (etak*cos(alphak)-(wsconv-Eqp)*sin(alphak) )./( (wsconv-Eqp).^2 + etak.^2);
+Aqpconv *= exp(-gammak)/pi;
+ddA     = dw*conv(Aqpconv, CS2conv);
+wsdd    = dw*[0:2*length(wsconv)-2]';
+wsdd   += 2*wsconv(1); 
+ddAI    = interp1(wsdd,ddA,wsC);
+A2      = A1 + ddAI;
+
+%# plasmon pole model
+[a,b] = max(Gamma);
+wmax  = ws(b);
+lambda= 0.002;
+dA3   = lambda/pi/(wmax-elda)^2 *Aqpw;
+wsPP  = ws-(wmax-elda);
+AqpPP = (etak*cos(alphak)-(wsPP-Eqp)*sin(alphak) )./( (wsPP-Eqp).^2 + etak.^2);
+AqpPP *= exp(-gammak)/pi;
+APP   = AqpPP + dA3;
